490a127049a51a745cb884e71fc53a265b436e22
N-itish <manandharnara@gmail.com>
2020-09-25 22:27:37 +0545

    testing vue

---
+++ testapp/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <button v-on:click ="hello">hello</button>
+  </div>
+</template>
+<script>
+export default {
+  name:"app",
+  methods:{
+    hello: function(){
+      alert("Hello World");
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
