647b54b440e9e6cfa0bcb7ed7593ffba2c347b3e
N-itish <manandharnara@gmail.com>
2020-09-27 12:57:38 +0545

    added table populated with array, whose rows can be individually deleted

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
-    <button v-on:click ="hello">hello</button>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>UserName</th>
+          <th>Department</th>
+          <th>Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user,index) in users" :key="index">
+          <td>{{index+1}}<td>
+          <td>{{user.username}}</td>
+          <td>{{user.department}}</td>
+          <td>{{user.address}}</td>
+          <button v-on:click = "remove(index)">delete</button>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
+
 export default {
   name:"app",
+  data(){
+     return {users: [
+      { username: 'Nitish',department:"IT",address:"Bagmati"},
+      { username: 'BoB',department:"RND",address:"Kumaripati"}
+    ]
+    }
+  },
   methods:{
-    hello: function(){
-      alert("Hello World");
+    remove: function(index){
+      this.users.splice(index,1);
     }
   }
 }
