3a29c58fd0589dcbcd564094ddf4c42aa0ccab02
N-itish <manandharnara@gmail.com>
2020-09-28 18:35:58 +0545

    use can dynamically add fields

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -17,6 +17,12 @@
           <td>{{user.address}}</td>
           <button v-on:click = "remove(index)">delete</button>
         </tr>
+        <tr>
+          <td><input v-model = "username" type = "text"/></td>
+          <td><input v-model = "department" type = "text"/></td>
+          <td><input v-model = "address" type="text"/></td>
+          <button v-on:click  ="add">add</button>
+        </tr>
       </tbody>
     </table>
   </div>
@@ -26,16 +32,21 @@
 export default {
   name:"app",
   data(){
-     return {users: [
-      { username: 'Nitish',department:"IT",address:"Bagmati"},
-      { username: 'BoB',department:"RND",address:"Kumaripati"}
-    ]
-    }
+    return {
+        users:[],
+        username:'',
+        department :'',
+        address:''
+      }
   },
   methods:{
     remove: function(index){
       this.users.splice(index,1);
     }
+    ,add: function(){
+      this.users.push({username:this.username,department:this.department,address:this.address});
+      console.log(this.username+" "+this.department+" "+this.address) 
+    },
   }
 }
 </script>
