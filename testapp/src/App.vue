1c9f24308d5e0543f6f9d01f7052bae969246663
N-itish <manandharnara@gmail.com>
2020-09-29 22:27:43 +0545

    using axios to get data then push it in the html

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -25,19 +25,28 @@
         </tr>
       </tbody>
     </table>
+    <h2>{{getData}}</h2>
   </div>
+
 </template>
 <script>
-
+import axios from 'axios';
 export default {
+  
+
   name:"app",
   data(){
     return {
         users:[],
         username:'',
         department :'',
-        address:''
+        address:'',
+        url:' https://cors-anywhere.herokuapp.com/https://postman-echo.com/get?foo1=bar1&foo2=bar2',
+        getData:[]
       }
+  },
+  mounted(){
+    this.getHttpRequest()
   },
   methods:{
     remove: function(index){
@@ -46,7 +55,12 @@ export default {
     ,add: function(){
       this.users.push({username:this.username,department:this.department,address:this.address});
       console.log(this.username+" "+this.department+" "+this.address) 
-    },
+    },getHttpRequest(){
+        axios.get(this.url).then((response) => {
+          console.log(response);
+          this.getData = response;
+      })
+    }
   }
 }
 </script>
